ce515cbe61ee2bcdf145a04de1087127bf9c12f2
basilive <sizov.basil@gmail.com>
2019-07-05 15:00:33 +0300

    Final

---
+++ scriptsheet.js
@@ -0,0 +1,23 @@
+// Burger-menu
+
+const navSlide = () => {
+    const burger = document.querySelector('.nav__burger');
+    const menu = document.querySelector('.nav__list');
+    const navItem = document.querySelectorAll('.nav__item');
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        menu.classList.toggle('nav__list--active');
+        burger.classList.toggle('nav__burger--active')
+        // Animate links In
+        navItem.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            } else {            
+                link.style.animation = `navItemFadeIn 0.5s ease forwards ${index / 7 + 0.6}s`;
+            }
+        });
+    });
+}
+
+navSlide();
